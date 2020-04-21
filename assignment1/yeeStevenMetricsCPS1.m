ce62667daa34b1f3579bee5d15fea134430cd607
syee <stevenyee64@gmail.com>
2020-04-20 19:34:22 -0700

    Revert "Functionalized"
    This reverts commit 152bf69398cabd67da99cfe00abecb194e90bc2d.

--- assignment1/yeeStevenMetricsCPS1.m
+++ assignment1/yeeStevenMetricsCPS1.m
@@ -4,12 +4,72 @@
 
 clear;
 clc;
+N = 500;
+T = 5;
+beta = 1;
+nreplic = 1000;
 
-[beta_hat5, sigma_beta_hat5, sigma_beta_tilde5, T] = fact(5,1);
+beta_hat = zeros(nreplic,1);
+
+%This is allowing for possible serial correlation
+sigma_beta_hat5 =  zeros(nreplic,1);
+%This is saying serial correlation is 0
+sigma_beta_tilde5 =  zeros(nreplic,1);
+
+for replic = 1:1:nreplic;
+    
+    %Create matrix of x values distributed standard normal into a T x N
+    %matrix
+    X = randn (T,N);
+    %sigma = repmat(sqrt(T)*mean(x),T,1);
+    %variance covariance matrix for u_{it}
+    sigma = abs(X);
+    E = sigma.*randn(T,N);
+    Y = X + E;
+    
+    %mean averages over each column. This gives us the average value for
+    %each individual. 
+    
+    %This is the demeaning
+    Y = Y - repmat(mean(Y),T,1);
+    E = E - repmat(mean(E),T,1);
+    X = X - repmat(mean(X),T,1);
+    
+    yy = Y';  % N x T
+    xx = X';  % N x T
+    ee = E';
+    
+    xx = xx(:); % vectorize x' 
+    yy = yy(:); % vectorize y'
+    ee = ee(:);
+
+    %beta_hat is like the beta from the pooled OLS
+    beta_hat(replic) = xx\yy;  %fixed effects estimator
+    %Equivalent method of calculating beta with pooled OLS
+    
+    beta_hat_molly = beta + sum(X.^2, 'all').^ -1 .*  sum(X.*E, 'all');
+
+    beta_hat_beta = beta + (sum(sum(X.^2,2),1)).^(-1) * sum(sum(X.*E,2),1);
+    
+%     beta_hat = xx\yy;
+    
+    beta_hat_ali = beta + norm(X,'fro')^(-2) *sum(sum((X).*(E)));
+    
+    ourBeta = beta + (sum(xx.^2).^-1) * sum(xx.*ee);
+    
+    % This is the demeaned error terms because it comes from the demeaned X
+    % and Y
+    e_hat = Y - X*beta_hat(replic);  % residual from fixed effect regression
+                                     % T x N
+    % equation 3
+    sigma_beta_hat5(replic) = sqrt((xx'*xx)^(-2)*sum(sum(X.*e_hat).^2));
+    % equation 4
+    sigma_beta_tilde5(replic) = sqrt((xx'*xx)^(-2)*sum(sum((X.^2).*(e_hat.^2))));
+end
 
 %1a
 figure(10);
-hist(beta_hat5);
+hist(beta_hat);
 title('4a T = 5 Distribution of Beta hat');
 saveas(gcf,'beta_hat_4a5.pdf');
 
@@ -24,7 +84,7 @@ title('4a T = 5 Distribution of Sigma tilde');
 saveas(gcf,'sigma_tilde_4a5.pdf');
 
 %1b
-true_sigma = std(beta_hat5);
+true_sigma = std(beta_hat);
 
 %1c
 sigma_hat5 = mean(sigma_beta_hat5);
@@ -46,12 +106,74 @@ display(['RMSE Tilde for T=', num2str(T), ': ', num2str(rmse_sigma_tilde5)]);
 display('Based on RMSE criterion, sigma_hat is better than sigma_tilde. Which');
 display('suggests there is some serial dependence in our data.')
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Problem 1d T = 10
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+N = 500;
+T = 10;
+beta = 1;
+nreplic = 1000;
 
-[beta_hat10, sigma_beta_hat10, sigma_beta_tilde10, T] = fact(10,1);
+beta_hat = zeros(nreplic,1);
 
+%This is allowing for possible serial correlation
+sigma_beta_hat10 =  zeros(nreplic,1);
+%This is saying serial correlation is 0
+sigma_beta_tilde10 =  zeros(nreplic,1);
+
+for replic = 1:1:nreplic;
+    
+    %Create matrix of x values distributed standard normal into a T x N
+    %matrix
+    X = randn (T,N);
+    %sigma = repmat(sqrt(T)*mean(x),T,1);
+    %variance covariance matrix for u_{it}
+    sigma = abs(X);
+    E = sigma.*randn(T,N);
+    Y = X + E;
+    
+    %mean averages over each column. This gives us the average value for
+    %each individual. 
+    
+    %This is the demeaning
+    Y = Y - repmat(mean(Y),T,1);
+    E = E - repmat(mean(E),T,1);
+    X = X - repmat(mean(X),T,1);
+    
+    yy = Y';  % N x T
+    xx = X';  % N x T
+    ee = E';
+    
+    xx = xx(:); % vectorize x' 
+    yy = yy(:); % vectorize y'
+    ee = ee(:);
+
+    %beta_hat is like the beta from the pooled OLS
+    beta_hat(replic) = xx\yy;  %fixed effects estimator
+    %Equivalent method of calculating beta with pooled OLS
+    
+    beta_hat_molly = beta + sum(X.^2, 'all').^ -1 .*  sum(X.*E, 'all');
+
+    beta_hat_beta = beta + (sum(sum(X.^2,2),1)).^(-1) * sum(sum(X.*E,2),1);
+    
+%     beta_hat = xx\yy;
+    
+    beta_hat_ali = beta + norm(X,'fro')^(-2) *sum(sum((X).*(E)));
+    
+    ourBeta = beta + (sum(xx.^2).^-1) * sum(xx.*ee);
+    
+    e_hat = Y - X*beta_hat(replic);  % residual from fixed effect regression
+                                     % T x N
+    % equation 3
+    sigma_beta_hat10(replic) = sqrt((xx'*xx)^(-2)*sum(sum(X.*e_hat).^2));
+    % equation 4
+    sigma_beta_tilde10(replic) = sqrt((xx'*xx)^(-2)*sum(sum((X.^2).*(e_hat.^2))));
+end
+
+%1d_a
 figure(10);
-hist(beta_hat5);
+hist(beta_hat);
 title('4d T = 10 Distribution of Beta hat');
 saveas(gcf,'beta_hat_4d10.pdf');
 
@@ -66,7 +188,7 @@ title('4d T = 10 Distribution of Sigma tilde');
 saveas(gcf,'sigma_tilde_4d10.pdf');
 
 %1d_b
-true_sigma = std(beta_hat5);
+true_sigma = std(beta_hat);
 
 %1d_c
 sigma_hat10 = mean(sigma_beta_hat10);
@@ -88,65 +210,26 @@ display(['RMSE Tilde for T=', num2str(T), ': ', num2str(rmse_sigma_tilde10)]);
 display('Based on RMSE criterion, sigma_hat is better than sigma_tilde. Which');
 display('suggests there is some serial dependence in our data.');
 
-
-[beta_hat20, sigma_beta_hat20, sigma_beta_tilde20, T] = fact(20,1);
-
-figure(10);
-hist(beta_hat5);
-title('4d T = 20 Distribution of Beta hat');
-saveas(gcf,'beta_hat_4d20.pdf');
-
-figure(1);
-hist(sigma_beta_hat20);
-title('4d T = 20 Distribution of Sigma hat');
-saveas(gcf,'sigma_hat_4d20.pdf');
-
-figure(2);
-hist(sigma_beta_tilde20);
-title('4d T = 20 Distribution of Sigma tilde');
-saveas(gcf,'sigma_tilde_4d20.pdf');
-
-%1d_b
-true_sigma = std(beta_hat5);
-
-%1d_c
-sigma_hat20 = mean(sigma_beta_hat20);
-sigma_tilde20 = mean(sigma_beta_tilde20);
-display(['Mean Hat for T=', num2str(T), ': ', num2str(sigma_hat20)]);
-display(['Mean Tilde for T=', num2str(T), ': ', num2str(sigma_tilde20)]);
-std_sigma_hat20 = std(sigma_beta_hat20);
-std_sigma_tilde20 = std(sigma_beta_tilde20);
-display(['Standard error Hat for T=', num2str(T), ': ', num2str(std_sigma_hat20)]);
-display(['Standard error Tilde for T=', num2str(T), ': ', num2str(std_sigma_tilde20)]);
-bias_hat20 = sigma_hat20 - true_sigma;
-bias_tilde20 = sigma_tilde20 - true_sigma;
-display(['Bias Hat for T=', num2str(T), ': ', num2str(bias_hat20)]);
-display(['Bias Tilde for T=', num2str(T), ': ', num2str(bias_tilde20)]);
-rmse_sigma_hat20 = sqrt((sigma_hat20-true_sigma)^2 + std_sigma_hat20^2);
-rmse_sigma_tilde20 = sqrt((sigma_tilde20-true_sigma)^2 + std_sigma_tilde20^2);
-display(['RMSE Hat for T=', num2str(T), ': ', num2str(rmse_sigma_hat20)]);
-display(['RMSE Tilde for T=', num2str(T), ': ', num2str(rmse_sigma_tilde20)]);
-display('Based on RMSE criterion, sigma_hat is better than sigma_tilde. Which');
-display('suggests there is some serial dependence in our data.');
-
 %Based on RMSE criterion, sigma_hat is better than sigma_tilde. Which
 %suggests there is some serial dependence in our data.
 
 
 
-function [beta_hat, sigma_beta_hat, sigma_beta_tilde, T] = fact(t, beta_val)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Problem 1d T = 20
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 N = 500;
-T = t;
-beta = beta_val;
+T = 20;
+beta = 1;
 nreplic = 1000;
 
 beta_hat = zeros(nreplic,1);
 
 %This is allowing for possible serial correlation
-sigma_beta_hat =  zeros(nreplic,1);
+sigma_beta_hat20 =  zeros(nreplic,1);
 %This is saying serial correlation is 0
-sigma_beta_tilde =  zeros(nreplic,1);
+sigma_beta_tilde20 =  zeros(nreplic,1);
 
 for replic = 1:1:nreplic;
     
@@ -187,14 +270,52 @@ for replic = 1:1:nreplic;
 %     
 %     ourBeta = beta + (sum(xx.^2).^-1) * sum(xx.*ee);
     
-    % This is the demeaned error terms because it comes from the demeaned X
-    % and Y
     e_hat = Y - X*beta_hat(replic);  % residual from fixed effect regression
                                      % T x N
     % equation 3
-    sigma_beta_hat(replic) = sqrt((xx'*xx)^(-2)*sum(sum(X.*e_hat).^2));
+    sigma_beta_hat20(replic) = sqrt((xx'*xx)^(-2)*sum(sum(X.*e_hat).^2));
     % equation 4
-    sigma_beta_tilde(replic) = sqrt((xx'*xx)^(-2)*sum(sum((X.^2).*(e_hat.^2))));
+    sigma_beta_tilde20(replic) = sqrt((xx'*xx)^(-2)*sum(sum((X.^2).*(e_hat.^2))));
 end
 
-end
+%1d_a
+figure(10);
+hist(beta_hat);
+title('4d T = 20 Distribution of Beta hat');
+saveas(gcf,'beta_hat_4d20.pdf');
+
+figure(1);
+hist(sigma_beta_hat20);
+title('4d T = 20 Distribution of Sigma hat');
+saveas(gcf,'sigma_hat_4d20.pdf');
+
+figure(2);
+hist(sigma_beta_tilde20);
+title('4d T = 20 Distribution of Sigma tilde');
+saveas(gcf,'sigma_tilde_4d20.pdf');
+
+%1d_b
+true_sigma = std(beta_hat);
+
+%1d_c
+sigma_hat20 = mean(sigma_beta_hat20);
+sigma_tilde20 = mean(sigma_beta_tilde20);
+display(['Mean Hat for T=', num2str(T), ': ', num2str(sigma_hat20)]);
+display(['Mean Tilde for T=', num2str(T), ': ', num2str(sigma_tilde20)]);
+std_sigma_hat20 = std(sigma_beta_hat20);
+std_sigma_tilde20 = std(sigma_beta_tilde20);
+display(['Standard error Hat for T=', num2str(T), ': ', num2str(std_sigma_hat20)]);
+display(['Standard error Tilde for T=', num2str(T), ': ', num2str(std_sigma_tilde20)]);
+bias_hat20 = sigma_hat20 - true_sigma;
+bias_tilde20 = sigma_tilde20 - true_sigma;
+display(['Bias Hat for T=', num2str(T), ': ', num2str(bias_hat20)]);
+display(['Bias Tilde for T=', num2str(T), ': ', num2str(bias_tilde20)]);
+rmse_sigma_hat20 = sqrt((sigma_hat20-true_sigma)^2 + std_sigma_hat20^2);
+rmse_sigma_tilde20 = sqrt((sigma_tilde20-true_sigma)^2 + std_sigma_tilde20^2);
+display(['RMSE Hat for T=', num2str(T), ': ', num2str(rmse_sigma_hat20)]);
+display(['RMSE Tilde for T=', num2str(T), ': ', num2str(rmse_sigma_tilde20)]);
+display('Based on RMSE criterion, sigma_hat is better than sigma_tilde. Which');
+display('suggests there is some serial dependence in our data.');
+
+%Based on RMSE criterion, sigma_hat is better than sigma_tilde. Which
+%suggests there is some serial dependence in our data.
